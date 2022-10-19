8d8c50de266b914c6c7bb0bc9ee4d695889ada31
Сергей Антонов <antonovseantonoff@yandex.ru>
2022-10-20 02:04:37 +0300

    publish photo to Telegram channel.

--- picture_to_telegram.py
+++ picture_to_telegram.py
@@ -7,10 +7,11 @@ import random
 def send_picture():
     random_number = random.choice(os.listdir("images"))
     load_dotenv()
+    chat_id = os.getenv('CHAT_ID')
     telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
     bot = telegram.Bot(token=telegram_bot_token)
     bot.send_document(
-        chat_id=-1001813178951, document=open(
+        chat_id=chat_id, document=open(
             f'images/{random_number}', 'rb')
     )
 
